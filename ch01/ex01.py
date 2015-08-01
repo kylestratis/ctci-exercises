02068437bb7acb2774360a528780afed3d24f78a
kylestratis <kylestratis@users.noreply.github.com>
2015-08-01 18:43:50 -0400

    Optimizations for exercise 1, such as using a set for the unique chars data structure

--- ch01/ex01.py
+++ ch01/ex01.py
@@ -6,22 +6,17 @@
 
 def main():
     test_data = ["Not unique", "Uniq"]
-    unique_characters(test_data[0])
-    unique_characters(test_data[1])
+    print unique_characters(test_data[0])
+    print unique_characters(test_data[1])
 
 def unique_characters(str):
-    unique_chars = []
-    nonunique_chars = []
+    unique_chars = set()
     for char in str:
         if char not in unique_chars:
-            unique_chars.append(char)
+            unique_chars.add(char)
         else:
-            nonunique_chars.append(char)
-    if len(nonunique_chars) > 0:
-        print "The string '%s' is NOT composed of unique characters." % str
-    else:
-        print "The string '%s' is composed of unique characters." % str
-        
+            return False
+    return True        
 
 if __name__ == "__main__":
     main()
