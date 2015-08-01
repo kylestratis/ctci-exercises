391846d14ebd5190509081b7e0010cd2360edec7
kylestratis <kylestratis@users.noreply.github.com>
2015-08-01 18:23:56 -0400

    Chapter 01 Exercise 04 - String replacement

---
+++ ch01/ex04.py
@@ -0,0 +1,14 @@
+# Chapter 01
+# Exercise 04
+# Write a method to replace all spaces in a string with '%20'
+# Pretty basic for Python
+
+def main():
+    test_string = "This is a test string"
+    print spaces(test_string)
+
+def spaces_easy(str):
+    return str.replace(' ','%20')
+
+if __name__ == "__main__":
+    main()
