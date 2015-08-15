1ccc3040dc172ce49170eb4de1244dc3b3fa3579
Kyle Stratis <kyle.stratis@gmail.com>
2015-08-14 21:33:15 -0400

    Added question 5 implementation, modified LinkedList generator to take input for range of numbers to generate

---
+++ c-sharp/ch02/Question05.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch02 {
+    /// <summary>
+    /// You have two numbers represented by a linked list, where each node contains a single digit. 
+    /// The digits are stored in reverse order, such that the 1's digit is at the head of the list. 
+    /// Write a function that adds the two numbers and returns the sum as a linked list.
+    /// E.g. IN: (7 -> 1 -> 6) + (5 -> 9 -> 2) => 617 + 295
+    ///     OUT: 2 -> 1 -> 9 => 912
+    /// </summary>
+    class Question05 {
+        public static void Execute(LinkedList<int> inputList1, LinkedList<int> inputList2) {
+            var newSum = AddReverseOrder(inputList1, inputList2);
+            Console.WriteLine("Chapter 02 Question 05 - Adding: ");
+            Console.Write("[");
+            foreach (var item in inputList1) {
+                Console.Write("{0} -> ", item); // Not the best way to present it, but oh well
+            }
+            Console.Write("] + [");
+            foreach (var item in inputList2) {
+                Console.Write("{0} -> ", item);
+            }
+            Console.Write("]");
+            Console.WriteLine(" = ");
+            Console.Write("[");
+            foreach (var item in newSum) {
+                Console.Write("{0} -> ", item);
+            }
+            Console.Write("]");
+            Console.WriteLine();
+        }
+
+        public static LinkedList<int> AddReverseOrder(LinkedList<int> a, LinkedList<int> b) {
+            int sum;
+            int carry = 0;
+            var linkedListSum = new LinkedList<int>();
+            var nodeA = a.First;
+            var nodeB = b.First;
+            while (nodeA != null || nodeB != null) {
+                sum = nodeA.Value + nodeB.Value + carry;
+                if (sum > 9) {
+                    carry = 1;
+                    sum -= 10;
+                }
+                else {
+                    carry = 0;
+                }
+                linkedListSum.AddLast(sum);
+                nodeA = nodeA.Next;
+                nodeB = nodeB.Next;
+            }
+            if (carry > 0) {
+                linkedListSum.AddLast(carry);
+            }
+            return linkedListSum;
+        }
+    }
+}
--- c-sharp/ch02/RunTests.cs
+++ c-sharp/ch02/RunTests.cs
@@ -13,19 +13,20 @@ namespace ch02 {
             dupeList.AddLast(9);
             dupeList.AddLast(13);
             Question01.Execute(dupeList);
-            Question02.Execute(GenerateLinkedList(7));
-            Question03.Execute(GenerateLinkedList(6)); // Return unchanged list
-            Question03.Execute(GenerateLinkedList(5));
-            Question04.Execute(GenerateLinkedList(8));
+            Question02.Execute(GenerateLinkedList(7, 200));
+            Question03.Execute(GenerateLinkedList(6, 200)); // Return unchanged list
+            Question03.Execute(GenerateLinkedList(5, 200));
+            Question04.Execute(GenerateLinkedList(8, 200));
+            Question05.Execute(GenerateLinkedList(3, 9), GenerateLinkedList(3, 9));
             Console.WriteLine("Press Enter to continue:");
             Console.Read();
         }
 
-        public static LinkedList<int> GenerateLinkedList(int size) {
+        public static LinkedList<int> GenerateLinkedList(int size, int max) {
             Random rand = new Random();
             var newList = new LinkedList<int>();
             for (int i = 0; i < size; i++) {
-                newList.AddLast(rand.Next(0, 299));
+                newList.AddLast(rand.Next(0, max));
                 }
             return newList;
         }
