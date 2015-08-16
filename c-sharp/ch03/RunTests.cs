be20caea8f2dcd6fb668f7a1f178a3d549c68c61
Kyle Stratis <kyle.stratis@gmail.com>
2015-08-16 18:47:27 -0400

    Implemented SetOfStacks class as per Ch 3 Q3, as well as a test harness.

---
+++ c-sharp/ch03/RunTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch03 {
+    class RunTests {
+        static void Main(string[] args) {
+            SetOfStacksTests();
+            Console.WriteLine("Press Enter to continue:");
+            Console.Read();
+        }
+
+        public static void SetOfStacksTests() {
+            var testSet = CreateSetOfStacks();
+            Console.WriteLine("Testing the functions of SetOfStacks");
+            Console.WriteLine("Our capacity per stack is {0}", testSet.Capacity);
+            Console.WriteLine("Popping 9 off stack should give: {0}", testSet.Pop());
+            Console.WriteLine("Pushing 3, 1, 2 onto set should give us a second Stack in the list");
+            testSet.Push(3);
+            testSet.Push(1);
+            testSet.Push(2);
+            Console.Write("[");
+            foreach (var item in testSet.Data[1]) {
+                Console.Write(" {0} ", item);
+            }
+            Console.Write("]");
+            Console.WriteLine(" ");
+        }
+
+        public static SetOfStacks CreateSetOfStacks() {
+            var setOfStacks = new SetOfStacks(3);
+            // First stack
+            setOfStacks.Push(1);
+            setOfStacks.Push(2);
+            setOfStacks.Push(3);
+            // Second stack
+            setOfStacks.Push(9);
+            return setOfStacks;
+
+        }
+    }
+}
---
+++ c-sharp/ch03/SetOfStacks.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch03 {
+    /// <summary>
+    /// Implement a datastructure that is composed of several stacks and will create a new one
+    /// once the previous reaches capacity. push() and pop() methods should act identical to 
+    /// those methods in a traditional stack. 
+    /// </summary>
+    class SetOfStacks {
+        public int Capacity { get; set; }
+        public List<Stack<int>> Data { get; set; }
+
+        private Stack<int> LastStack {
+            get { return Data[Data.Count - 1]; }
+        }
+
+        public SetOfStacks(int capacity) {
+            Capacity = capacity;
+            Data = new List<Stack<int>>();
+            Data.Add(new Stack<int>());
+        }
+
+        public int Pop() {
+            var stack = LastStack;
+            var popped = stack.Pop();
+            if (stack.Count == 0) {
+                Data.Remove(stack);
+            }
+            return popped;
+        }
+
+        public void Push(int pushed) {
+            var stack = LastStack;
+            if (stack.Count >= Capacity && (stack != null)) {
+                var newStack = new Stack<int>();
+                newStack.Push(pushed);
+                Data.Add(newStack);
+            }
+            else if (stack != null) {
+                stack.Push(pushed);
+            }
+
+        }
+    }
+}
